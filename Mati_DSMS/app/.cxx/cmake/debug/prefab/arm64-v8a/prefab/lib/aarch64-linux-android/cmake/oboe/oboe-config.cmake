e5c3afb5c570b6874e88c84719c75ae0646d047a
tarzan <gogikar.akhilesh@gmail.com>
2021-02-12 22:51:39 -0800

    Added working Mati DSMS

--- Mati_DSMS/app/.cxx/cmake/debug/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/oboe/oboe-config.cmake
+++
@@ -1,7 +0,0 @@
-add_library(oboe::oboe SHARED IMPORTED)
-set_target_properties(oboe::oboe PROPERTIES
-    IMPORTED_LOCATION "/home/tarzan/.gradle/caches/transforms-2/files-2.1/6cafa8e34088341aecd818f197cc9c67/oboe-1.5.0/prefab/modules/oboe/libs/android.arm64-v8a/liboboe.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/tarzan/.gradle/caches/transforms-2/files-2.1/6cafa8e34088341aecd818f197cc9c67/oboe-1.5.0/prefab/modules/oboe/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-
